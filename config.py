07d054faab7e941564e3baf2ed97791efa06191f
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2025-10-06 11:13:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
 PROJECT_NAME = "GitGram"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/tucommenceapousser/GitGram"
